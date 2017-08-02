f440920ff7d433cf3656d3c55fdd03ca9c4eb5fd
Chadwick D Huston <chadhuston@gmail.com>
2017-08-02 16:56:29 -0500

    Ch 4. draft
    working on assembly language stuff

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Adds 1+...+100.
+@i // i refers to some mem. location.
+M=1 // i=1
+@R3 // R3refers to some mem. location.
+M=0 // R3=0
+(LOOP)
+@i
+D=M // D=i
+@R1
+D=D-A // D=i-R1
+@END
+D;JGT // If (i-R1)>0 goto END
+@i
+D=M // D=i
+@R3
+M=D+M // R3=R3+R2
+@i
+M=M+1 // i=i+1
+@LOOP
+0;JMP // Goto LOOP
+(END)
+@END
+0;JMP // Infinite loop
---
+++ 04/mult/multiply.py
@@ -0,0 +1,5 @@
+def multiply(r0,r1):
+	r3 = 0
+	for x in range(r0):
+		r3=r3+r1
+	return r3
